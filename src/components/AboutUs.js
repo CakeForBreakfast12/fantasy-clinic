92fa5c18e566d44bc0e513d6020828d96d4086f4
Precup <uidu3501@contiwan.com>
2019-12-18 11:39:53 +0200

    vacanta

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,11 +3,31 @@ import React from 'react';
 
 
 const AboutUs = () => (
-    <header>
-        <h3>About Us</h3>
+    <div className="about-us">
+        <h1>Our Mission</h1>
+        <p>UCSF Health's mission, vision and values convey the reasons we exist as an organization, what we aspire to as an organization and the way we will work to get there. They are a unifying force that link all of us together.</p>
+        <h2>Mission</h2>
+        <p>Our mission — the reason we exist — is Caring, Healing, Teaching and Discovering.</p>
+        <h2>Vision</h2>
+        <p>Our vision — what we want to be — is to be the best provider of health care services, the best place to work and the best environment for teaching and research.</p>
+        <h2>Values</h2>
+        <p>Our values statement — our guide to the individual and organizational behavior we expect — is embodied in the acronym PRIDE:
+
+            P for Professionalism, how we conduct ourselves and our business
+            R for Respect for our patients, families, ourselves and each other
+            I for Integrity, always doing the honest, right thing
+            D for Diversity, understanding and embracing the diverse beliefs, needs and expectations of our patients, community and employees
+            E for Excellence, what we strive for in everything we do
+        </p>
+        <h2>Welcome All</h2>
+        <p>UCSF Health treats all patients who require our services, without regard to race, color, religion, national origin, citizenship or other protected characteristics. Our UC medical centers remain committed to these responsibilities and will vigorously enforce university nondiscrimination and privacy policies. We will not release immigration related information to federal agencies or others without a warrant or subpoena or as otherwise required by law, and will work diligently to protect the privacy of all members of our community. For further information, read UC's full statement in support of undocumented members of our community or review a detailed FAQ.</p>
+
+    </div>
 
 
-    </header>
+
+
+
 );
 
 
