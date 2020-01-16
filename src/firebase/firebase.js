25a8c6cf82c4b3ae2888f0677b8777e1692770a9
Precup <uidu3501@contiwan.com>
2020-01-16 15:32:58 +0200

    firebase not working in Heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: "AIzaSyCqAmPcwV7h9h2yGLEJrPpT-UUVn-GTChA",
+    authDomain: "fantasy-clinic.firebaseapp.com",
+    databaseURL: "https://fantasy-clinic.firebaseio.com",
+    projectId: "fantasy-clinic",
+    storageBucket: "fantasy-clinic.appspot.com",
+    messagingSenderId: "1011134547778",
+    appId: "1:1011134547778:web:86c14422d73bb669dc6adb"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -24,7 +24,13 @@ export { firebase, database as default }
 
 
 
-
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.FIREBASE_DATABASE_URL,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID
 
 
 
